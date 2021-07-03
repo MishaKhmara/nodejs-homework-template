91e8af1f69a6a623726922058e0d391165daff2e
MishaKhmara <1927mishakhmara03@gmail.com>
2021-07-03 15:59:04 +0300

    Шаг 4

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,7 +4,7 @@ const AuthCtrl = require('../../controllers/UserCtrl');
 const AvatarCtrl = require('../../controllers/AvatarCtrl');
 const Auth = require('./userAuth');
 const { ValidateAuth } = require('../../service/validation');
-// const upload = require('../../service/upload');
+const upload = require('../../service/upload');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs/promises');
@@ -16,51 +16,11 @@ router.post('/auth/login', ValidateAuth, AuthCtrl.login);
 router.post('/auth/logout', Auth, AuthCtrl.logout);
 router.get('/auth/current', Auth, AuthCtrl.currentUser);
 
-// router.patch(
-//   '/avatars',
-//   Auth,
-//   upload.single('avatar'),
-//   AvatarCtrl.uploaderCtrl,
-// );
-
-const tempDir = path.join(process.cwd(), 'public');
-const uploadDir = path.join(process.cwd(), 'public', 'avatars');
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, tempDir);
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  },
-  limits: {
-    fileSize: 10000000,
-  },
-});
-
-const upload = multer({
-  storage,
-});
-
-router.patch('/avatars', upload.single('avatar'), async (req, res, next) => {
-  const { path: tempName, originalname } = req.file;
-  const fileName = path.join(uploadDir, originalname);
-
-  try {
-    await fs.rename(tempName, fileName);
-    res.json({
-      statsus: 'success',
-      code: 200,
-      data: {
-        result: {
-          avatar: fileName,
-        },
-      },
-    });
-  } catch (error) {
-    await fs.unlink(tempName);
-    next(error);
-  }
-});
+router.patch(
+  '/avatars',
+  Auth,
+  upload.single('avatar'),
+  AvatarCtrl.uploaderCtrl,
+);
 
 module.exports = router;
--- service/upload.js
+++ service/upload.js
@@ -1,7 +1,6 @@
 const multer = require('multer');
 const path = require('path');
-
-const tempDir = path.join(process.cwd(), '../public/avatars');
+const tempDir = path.join(process.cwd(), 'tmp');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
