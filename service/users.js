ee4748f19dd8f00a6a9f948a83498fd6aa34ef14
MishaKhmara <1927mishakhmara03@gmail.com>
2021-07-09 13:52:24 +0300

    last Commit

--- service/users.js
+++ service/users.js
@@ -1,4 +1,6 @@
 const User = require('../model/schemas/UserSchema');
+const sendEmail = require('../service/email');
+const { nanoid } = require('nanoid');
 
 const getOne = filter => {
   return User.findOne(filter);
@@ -9,12 +11,29 @@ const getById = id => {
 };
 
 const add = async ({ email, password }) => {
-  const user = new User({ email, password });
+  const verifyToken = nanoid();
+  const user = await new User({
+    email,
+    password,
+    verification: false,
+    verifyToken,
+  });
+  await sendEmail(verifyToken, email);
   return await user.save();
 };
 
 const update = async (id, token) => {
   return await User.updateOne({ _id: id }, { token });
+};
+const findByVerificationToken = async verifyToken => {
+  return await User.findOne({ verifyToken });
+};
+
+const updateVerificationToken = async (userId, verification, verifyToken) => {
+  return await User.findByIdAndUpdate(
+    { _id: userId },
+    { verification, verifyToken },
+  );
 };
 
 module.exports = {
@@ -22,4 +41,6 @@ module.exports = {
   add,
   getById,
   update,
+  findByVerificationToken,
+  updateVerificationToken,
 };
